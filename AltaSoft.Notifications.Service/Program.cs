e550fafc3b927e13b53da40f5eaba23c69bb598f
Ezeki Zibzibadze <ez@jok.io>
2015-10-02 16:20:18 +0400

    updated service to TopShelf

--- AltaSoft.Notifications.Service/Program.cs
+++ AltaSoft.Notifications.Service/Program.cs
@@ -1,44 +1,51 @@
-﻿using AltaSoft.Notifications.Service.Common;
-using AltaSoft.Notifications.Service.Service;
-using Microsoft.Owin.Hosting;
+﻿using Microsoft.Owin.Hosting;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.ComponentModel.Composition.Hosting;
 using System.Configuration;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+using Topshelf;
 
 namespace AltaSoft.Notifications.Service
 {
     class Program
     {
+        static WorkerManager normalWorker, highPriorityWorker;
+
         static void Main(string[] args)
         {
-            if (!Environment.UserInteractive)
+            HostFactory.Run(x =>
             {
-                ServiceBase.Run(new[] { new NotificationsService() });
-                return;
-            }
+                x.Service<object>(s =>
+                {
+                    s.ConstructUsing(name => new object());
+                    s.WhenStarted(tc => Start());
+                    s.WhenStopped(tc => Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Manages communication to providers.");
+                x.SetDisplayName("AltaSoft Notifications Service");
+                x.SetServiceName("Notifications.Service");
+            });
+        }
 
 
+        static void Start()
+        {
             var url = ConfigurationManager.AppSettings["SignalrUrl"];
-            var worker = new WorkerManager(DAL.MessagePriority.Normal);
-            var worker2 = new WorkerManager(DAL.MessagePriority.High);
 
-            worker.Start();
-            worker2.Start();
+            normalWorker = new WorkerManager(DAL.MessagePriority.Normal);
+            highPriorityWorker = new WorkerManager(DAL.MessagePriority.High);
 
-            using (WebApp.Start(url))
-            {
-                Console.WriteLine("Server running on {0}", url);
-                Console.ReadLine();
-            }
+            normalWorker.Start();
+            highPriorityWorker.Start();
 
+            WebApp.Start(url);
+        }
+
+        static void Stop()
+        {
+            normalWorker.Stop();
+            highPriorityWorker.Stop();
+            Environment.Exit(0);
         }
     }
 }
